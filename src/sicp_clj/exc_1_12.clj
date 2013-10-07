a7ccdcf857dc816306752751f514b6eece3c8475
Joe Wilner <jhwilner1@gmail.com>
2013-10-07 15:51:49 -0400

    Updated through exercise 16

---
+++ src/sicp_clj/exc_1_12.clj
@@ -0,0 +1,21 @@
+(ns sicp-clj.exc_1_12)
+
+(defn pascal [x y]
+  (cond
+    (and (= x 0) (= y 0)) 1
+    (or (> y x) (< y 0)) 0
+    :else (+ (pascal (dec x) (dec y))
+             (pascal (dec x) y))))
+
+(defn p [limit]
+  (do 
+    (defn p-loop [row col]
+      (do
+        (println (pascal row col))
+        (cond
+          (and (= row limit) (= col limit)) nil
+          (= row col) (p-loop (inc row) 0)
+          :else (p-loop row (inc col)))))
+    (p-loop 0 0)))
+
+(p 4)
