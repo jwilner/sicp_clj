3063e9060c2511ffa5a4747561e33b5988451651
Joe Wilner <jhwilner1@gmail.com>
2013-10-10 17:34:24 -0400

    Ugh. Actual corrected commit. I need to learn how to do those fix commits.

--- src/sicp_clj/exc_1_24.clj
+++ src/sicp_clj/exc_1_24.clj
@@ -2,7 +2,7 @@
   (:use [sicp-clj.exc_1_22 :only [prime-search-wrap]]
         [sicp-clj.example_fermat_test :only [fast-prime?]]))
 
-(defn fast-prime-five? [n]
-  (fast-prime? n 5))
+(defn fast-prime-fermat [n]
+  (fast-prime? n 3))
 
 (defn main [] (prime-search-wrap fast-prime-five?))
