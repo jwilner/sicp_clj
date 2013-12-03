c346d032b3eb50718e0467faf89caa04fd923a05
Joe Wilner <jhwilner1@gmail.com>
2013-12-03 15:34:30 -0500

    Fixed a mistake in 60

--- src/sicp_clj/ch2/60.clj
+++ src/sicp_clj/ch2/60.clj
@@ -1,7 +1,7 @@
 (ns sicp-clj.ch2.60)
 
-;; just change union-set to append?
+;; just change union-set to append, adjoin-set to conj?
 ;; element-of-set is still O(n)
-;; as is adjoin-set
+;; adjoin-set becomes O(1)
 ;; intersection-set is just O(n2)
 ;; union-set changes to whatever append is
